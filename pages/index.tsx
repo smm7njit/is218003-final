d7ca0ccd25b173897acab421a8112ae0078f1723
Mourtada <smm7@njit.edu>
2023-12-16 13:30:37 -0500

    Add template code for home pages as per issue #17
    Added template code for index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Container, Navbar, Text, Button, Grid, Col } from '@nextui-org/react';
+import InfoCard from '../components/InfoCard';
+import { Table } from "@nextui-org/react";
+import Link from 'next/link';
+import {Textarea} from "@nextui-org/react";
+import NewsletterForm from '../components/NewsletterForm'
+import CookieConsent from 'react-cookie-consent';
+import Header from '../components/Header';
+
+
+
+
+
+const Home: NextPage = () => {
+
+}
+
+export default Home
