1cfc5da6ee5bbc8da1c4a49336d5cc1856e30389
Mourtada <smm7@njit.edu>
2023-12-18 05:39:17 -0500

    Add playwright test as per issue #47

--- e2e/test-2.spec.ts
+++ e2e/test-2.spec.ts
@@ -5,6 +5,11 @@ test('Test hero section text is present', async ({ page }) => {
   await page.getByText('When it comes to beverages,').click();
 });
 
+test('Check link to Newsletter works', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: 'To Our Newsletter' }).click();
+});
+
 
 
 
